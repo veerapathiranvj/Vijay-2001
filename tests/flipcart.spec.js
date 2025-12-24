459acad94e665363217b30b790edf9b37d39a158
veerapathiranvj <veerapathiran4057@gmail.com>
2025-12-24 20:38:40 +0530

    tests deleted

--- tests/flipcart.spec.js
+++
@@ -1,6 +0,0 @@
-const{test,expect}= require('@playwright/test')
-test('flipcart',async({page})=>{
-    await page.goto('https://www.flipkart.com/');
-    await page.locator('//img[@title="Flipkart"]')
-    await expect(page.locator('//img[@title="Flipkart"]')).toBeVisible()
-})
