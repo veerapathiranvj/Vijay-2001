fd8c464f08987abaafd4b3fe8d6ece541b588d18
veerapathiranvj <veerapathiran4057@gmail.com>
2025-12-25 12:48:38 +0530

    delete panniyachu

--- playwright-practice/tests/API.js
+++
@@ -1,9 +0,0 @@
-const requestOptions = {
-  method: "GET",
-  redirect: "follow"
-};
-
-fetch("https://automationexercise.com/api/productsList", requestOptions)
-  .then((response) => response.text())
-  .then((result) => console.log(result))
-  .catch((error) => console.error(error));
--- playwright-practice/tests/datepickers3.spec.js
+++
@@ -1,9 +0,0 @@
-const{test,expect}=require('@playwright/test')
-test('3rd model',async({page})=>{
-
-    await page.goto('https://testautomationpractice.blogspot.com/');
-     await page.locator('[id="comboBox"]').click();
-     await page.selectOption('')
-    await page.waitForTimeout(5000)
-
-})
