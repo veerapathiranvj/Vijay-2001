a5fbaf08e967d6d7be49e7e689f6271b995247cf
veerapathiranvj <veerapathiran4057@gmail.com>
2025-12-25 14:03:45 +0530

    del panniyachu

--- repo2/tests/upload.spec.js
+++
@@ -1,5 +0,0 @@
-import{test,expect} from "@playwright/test"
-test('upload',async({page})=>{
-    await page.goto('http://qa-automation-practice.netlify.app/file-upload.html');
-    await expect(page.locator('//h2[text()="File Upload Example"]')).toHaveText('File Upload Example') 
-}) //tohave text ku ull expect pandra text kudukanum 
